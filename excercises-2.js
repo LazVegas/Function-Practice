ccf636f51bcf3a41da14cc48eb01632b3882f840
Punchcode <lazaruslyon42@gmail.com>
2019-10-08 16:38:30 -0700

    Functions exercise

--- excercises-2.js
+++ excercises-2.js
@@ -4,6 +4,14 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
+function max (num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,6 +20,16 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+
+function maxOfThree(n1, n2, n3) {
+    if (n1 > n2 && n1 > n3) {
+        return n1;
+    } else if (n2 > n1 && n2 > n3) {
+        return n2;
+    } else {
+        return n3;
+    }
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
@@ -22,6 +40,15 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // string of length 1) and returns true if it is a 
 // vowel, false otherwise.
 // ---------------------
+
+function isVowel(x){
+        if (x == "A" || x == "E" || x == "I" || x == "O" || x == "U" || x == "a" || x == "e" || x == "i" || x == "o" || x == "u") {
+            return true;
+        }
+        else {
+            return false;
+    }
+}
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -38,6 +65,18 @@ console.assert(isVowel("2") === false)
 // the string "tothohisos isos fofunon".
 // ---------------------
 
+function rovarspraket(str) {
+    var newWord = "";
+    for (var i = 0 ; i < str.length; i++) {
+        if (str.charAt(i) == "A" || str.charAt(i) == "E" || str.charAt(i) == "I" ||         str.charAt(i) == "O" || str.charAt(i) == "U" || str.charAt(i) == "a" ||          str.charAt(i) == "e" || str.charAt(i) == "i" || str.charAt(i) == "o" ||          str.charAt(i) == "u") {
+            newWord += str.charAt(i);
+        } else {
+            newWord += str.charAt(i) + "o" + str.charAt(i);
+        }
+    }   
+    return newWord;
+}
+
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
 console.assert(rovarspraket("cat") === "cocatot")
@@ -51,6 +90,13 @@ console.assert(rovarspraket(0) === "0")
 // return the string "ratset gaj".
 // ---------------------
 
+function reverse(str){
+    let newWord = "";    
+    for (var i = str.length - 1; i >= 0; i--){        
+      newWord += str[i];
+    }
+    return newWord;
+  }
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
